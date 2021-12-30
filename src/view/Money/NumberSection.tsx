af3a1a9358439ecf480f43c6c0d3252dd81d4ca3
mamenglu <2245468037@qq.com>
2021-12-30 22:38:57 +0800

    数字面板基本完成

--- src/view/Money/NumberSection.tsx
+++ src/view/Money/NumberSection.tsx
@@ -57,26 +57,51 @@ const Warrper = styled.section`
    }
 `;
 const NumberSection: React.FC = () => { 
-  const [output, setOutput] = useState("0")
+  const [output, _setOutput] = useState("0")
+  const setOutput=( output:string ) => {
+    if (output.length > 16) {
+      output=output.slice(0, 16)
+    } else if (output.length === 0) { 
+      output="0"
+    }
+ _setOutput(output)
+  }
   const addNumber = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
-    console.log(text);
-  // console.log(e.target);
     if (text === null) { return };
-    switch(text){
+    switch (text) {
+      case "0":
+      case "1":
+      case "2":
       case "3":
-          // setOutput(text);
-        console.log(text);
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        if (output === "0") {
+          setOutput(text)
+        } else { 
+          setOutput(output+text)
+        };
         break;
+      case ".":
+        if (output.indexOf(".")>=0) { return }
+        else {  setOutput(output + ".");}
+        break;  
       case "删除":
-        console.log("删除");
+        if (output.length === 1) {
+          setOutput("")
+         }
+        else { setOutput(output.slice(0, -1)) };
         break;
       case "OK":
         console.log("OK");
         break;   
         case "清空":
-        console.log("清空");
-        break;    
+        setOutput("");
+        break;  
    }
   }
   return (
@@ -85,20 +110,20 @@ const NumberSection: React.FC = () => {
         { output }
         </div>  
       <div className='pad clearfix' onClick={addNumber}>
-          <button> 1</button>
-          <button> 2</button>
-          <button> 3</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
           <button>删除</button>
-          <button> 4</button>
-          <button> 5</button>
-          <button> 6</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
           <button>清空</button>
-          <button> 7</button>
-          <button> 8</button>
-          <button> 9</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
            <button className='ok'>OK</button>
-          <button className='zero'> 0</button>
-          <button> .</button>
+          <button className='zero'>0</button>
+          <button>.</button>
         </div>
 </Warrper>
 
