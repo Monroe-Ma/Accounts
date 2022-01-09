ee38e64002eed709246fff1d93f605e8262c7757
mamenglu <2245468037@qq.com>
2022-01-09 14:25:08 +0800

    展示分组

--- src/view/Statistics.tsx
+++ src/view/Statistics.tsx
@@ -3,7 +3,7 @@ import Layout from 'components/Layout';
 import CategorySection from './Money/CategorySection';
 import { useState } from 'react';
 import styled from 'styled-components';
-import { useRecords } from 'hooks/useRecords';
+import { useRecords,RecordsItem } from 'hooks/useRecords';
 import useTags from 'hooks/useTags';
 import day from "dayjs";
 
@@ -32,36 +32,56 @@ padding: 10px 10px;
     font-weight: 600;
   }
 `;
-
+const Header = styled.h3`
+padding: 10px ;
+`;
 function Statistics() {
   const [category, setCategory] = useState<"+" | "-">("-")
   const { records } = useRecords()
-  const {getTagName} =useTags()
+  const { getTagName } = useTags()
+  const selectedCategory = records.filter(r => { 
+   return  r.category === category
+  })
+  const hash: { [K: string]: RecordsItem[] } = {};
+  selectedCategory.forEach(r=>{
+    const key = day(r.createdAt).format("YYYY-MM-DD")
+    if (!(key in hash)) { 
+      hash[key]=[]
+    }
+    hash[key].push(r)
+  })
+  const array = Object.entries(hash).sort((a,b)=>{
+  if (a[0] === b[0]) return 0;
+  if (a[0] > b[0]) return -1;
+    if (a[0] < b[0]) return 1;
+    return 0;
+  })
+  console.log(array);
+  
   return (
     <Layout>
       <CategorySectionWrapper>
         <CategorySection value={category} onChange={value => setCategory(value)} />
       </CategorySectionWrapper>
-      <div >
-        {records.map(r => { 
+      {array.map(([date,records]) => 
+        <div>
+          <Header>
+            {date}
+            </Header>
+          {records.map(r => { 
           return <Item >
-       
             <ul >
              <li> {r.tagIds.map(tagId => { return <span >{getTagName(tagId)}</span> })}</li>
-              
               <li>
-                <span className='textContent'> {day(r.createdAt).format("YYYY年MM月DD日")}</span>
+                <span className='textContent'> {day(r.createdAt).format("HH:mm:ss")}</span>
                 <span className='textContent'>{r.note} </span>
               </li>
               </ul>
-    
               <div>￥{r.amount}</div>
-              
-      
           </Item>
            })}
-     </div>
-            
+        </div>
+      )}
     </Layout>
   );
 }
